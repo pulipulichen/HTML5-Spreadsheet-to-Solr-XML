846d7770291b296521a1c72b3c9867271393c8e4
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-10-25 17:01:35 +0800

    1025 調整功能

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 An HTML5 widget for converting a spreadsheet file to Solr XML format.
 
 https://pulipulichen.github.io/HTML5-Spreadsheet-to-Solr-XML/
+
+http://l.pulipuli.info/17/solr-xml
+
+http://blog.pulipuli.info/2017/10/apache-solr-apache-solr-build-fulltext.html
--- import_xlsxworker2.js
+++ import_xlsxworker2.js
@@ -1,50 +1,57 @@
 // Build a worker from an anonymous function body
-var blobURL = URL.createObjectURL( new Blob([ '(',
+var blobURL = URL.createObjectURL(new Blob(['(',
 
-function(){
-    //Long-running work here
+    function () {
+        //Long-running work here
 
-/* xlsx.js (C) 2013-2014 SheetJS -- http://sheetjs.com */
-/* uncomment the next line for encoding support */
-//importScripts('dist/cpexcel.js');
-try {
-    importScripts('https://pulipulichen.github.io/HTML5-Spreadsheet-to-Solr-XML/jszip.js');
-} catch (e) {}
-try {
-    importScripts('https://pulipulichen.github.io/HTML5-Spreadsheet-to-Solr-XML/xlsx.js');
-} catch (e) {}
-/* uncomment the next line for ODS support */
-try {
-    importScripts('https://pulipulichen.github.io/HTML5-Spreadsheet-to-Solr-XML/ods.js');
-} catch (e) {}
-postMessage({t:"ready"});
+        /* xlsx.js (C) 2013-2014 SheetJS -- http://sheetjs.com */
+        /* uncomment the next line for encoding support */
+        //importScripts('dist/cpexcel.js');
+        try {
+            importScripts('https://pulipulichen.github.io/HTML5-Spreadsheet-to-Solr-XML/jszip.js');
+        } catch (e) {
+        }
+        try {
+            importScripts('https://pulipulichen.github.io/HTML5-Spreadsheet-to-Solr-XML/xlsx.js');
+        } catch (e) {
+        }
+        /* uncomment the next line for ODS support */
+        try {
+            importScripts('https://pulipulichen.github.io/HTML5-Spreadsheet-to-Solr-XML/ods.js');
+        } catch (e) {
+        }
+        postMessage({t: "ready"});
 
-function ab2str(data) {
-	var o = "", l = 0, w = 10240;
-	for(; l<data.byteLength/w; ++l) o+=String.fromCharCode.apply(null,new Uint16Array(data.slice(l*w,l*w+w)));
-	o+=String.fromCharCode.apply(null, new Uint16Array(data.slice(l*w)));
-	return o;
-}
+        function ab2str(data) {
+            var o = "", l = 0, w = 10240;
+            for (; l < data.byteLength / w; ++l)
+                o += String.fromCharCode.apply(null, new Uint16Array(data.slice(l * w, l * w + w)));
+            o += String.fromCharCode.apply(null, new Uint16Array(data.slice(l * w)));
+            return o;
+        }
 
-function s2ab(s) {
-  var b = new ArrayBuffer(s.length*2), v = new Uint16Array(b);
-  for (var i=0; i != s.length; ++i) v[i] = s.charCodeAt(i);
-  return [v, b];
-}
+        function s2ab(s) {
+            var b = new ArrayBuffer(s.length * 2), v = new Uint16Array(b);
+            for (var i = 0; i !== s.length; ++i)
+                v[i] = s.charCodeAt(i);
+            return [v, b];
+        }
 
-onmessage = function (oEvent) {
-  var v;
-  try {
-    console.log(oEvent.data);
-    v = XLSX.read(ab2str(oEvent.data), {type: 'binary'});
-  } catch(e) { postMessage({t:"e",d:e.stack}); }
-  var res = {t:"xlsx", d:JSON.stringify(v)};
-  var r = s2ab(res.d)[1];
-  postMessage(r, [r]);
-};
+        onmessage = function (oEvent) {
+            var v;
+            try {
+                console.log(oEvent.data);
+                v = XLSX.read(ab2str(oEvent.data), {type: 'binary'});
+            } catch (e) {
+                postMessage({t: "e", d: e.stack});
+            }
+            var res = {t: "xlsx", d: JSON.stringify(v)};
+            var r = s2ab(res.d)[1];
+            postMessage(r, [r]);
+        };
 
-}.toString(),
+    }.toString(),
 
-')()' ], { type: 'application/javascript' } ) );
+    ')()'], {type: 'application/javascript'}));
 
-xlsxworker2 = new Worker( blobURL );
+xlsxworker2 = new Worker(blobURL);
